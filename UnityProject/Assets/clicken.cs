095035f1382e2ccea38b13a51beb299c1c7087b8
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-10-17 12:08:38 +0800

    13

---
+++ UnityProject/Assets/clicken.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clicken : MonoBehaviour {
+
+    Animator myAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+            Debug.Log("123");
+        myAnimator = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //吃飯
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            myAnimator.SetTrigger("吃飯觸發器");
+            Debug.Log("123");
+        }
+        //跑步
+        if (Input.GetKey(KeyCode.R))
+        {
+            myAnimator.SetBool("跑步開關", true);
+        }
+        else
+        {
+            myAnimator.SetBool("跑步開關", false);
+        }
+	}
+}
